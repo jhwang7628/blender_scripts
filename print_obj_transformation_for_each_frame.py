013b46f2c9553cb5f1d5d8ef44a7fda100a7e753
Jui-Hsien Wang <jhwang.cornell@gmail.com>
2017-12-11 16:00:35 -0800

    use translation/quaternion and transform the z-up to y-up

--- print_obj_transformation_for_each_frame.py
+++ print_obj_transformation_for_each_frame.py
@@ -1,26 +1,27 @@
 import bpy 
+import bpy_extras
 
-##
-filename = 'box_transformation.kin'
-
-##
 sce = bpy.context.scene
 obj = bpy.context.object
 
-with open(filename, 'w') as stream: 
+with open('leroy_box_top_y-up.kin', 'w') as stream: 
     for f in range(sce.frame_start, sce.frame_end+1): 
         sce.frame_set(f)
         print ('=== Frame %u' %(f)) 
     
-        mat = bpy.context.object.matrix_world 
-        print (mat)
+        global_matrix = bpy_extras.io_utils.axis_conversion(to_forward="-Z", to_up="Y").to_4x4()
+        print (global_matrix)
+        mat = global_matrix * bpy.context.object.matrix_world * global_matrix.transposed()
         entries = []
         for ii in range(4): 
             for jj in range(4): 
                 entries.append(mat[ii][jj])
-        mat_str = (' ').join([str('%.12f'%(x)) for x in entries])  
-        line = '%u %s\n' %(f, mat_str)
+        # mat_str = (' ').join([str('%.12f'%(x)) for x in entries])  
+        # line = '%u %s\n' %(f, mat_str)
+        
+        tra, rot, sca = mat.decompose()
+        # format: frame, translate_x, translate_y, translate_z, quaternion_w, quaternion_x, quaternion_y, quaternion_z
+        line = '%u %.12f %.12f %.12f %.12f %.12f %.12f %.12f\n' %(f, tra[0], tra[1], tra[2], rot.w, rot.x, rot.y, rot.z)
+        
         print (line)
         stream.write(line)
-        
-                
